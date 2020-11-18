63f3daee10db6a583f370ec68afffd517bd9f1ae
ImYuanYuan <10836016@ntub.edu.tw>
2020-11-18 17:48:34 +0800

    fix : 修正GetSQLValueString()語法錯誤及PHP版本更新失敗
    問題
    在GetSQLValueString()裡面找不到資料庫連線資料
    composer.json 無法降版
    
    調整項目
    新增$cralwer參數進GetSQLValueString()裡面
    刪除composer.json 內容 , 刪除composer.lock檔案

--- rent/home.php
+++ rent/home.php
@@ -29,31 +29,31 @@ if ((isset($_GET['doLogout'])) && ($_GET['doLogout'] == "true")) {
 ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+    function GetSQLValueString($cralwer, $theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
     {
-        $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+      $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
 
-        $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($cralwer,$theValue) : mysqli_escape_string($cralwer,$theValue);
+      $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($cralwer, $theValue) : mysqli_escape_string($cralwer, $theValue);`enter code here`
 
-        switch ($theType) {
-            case "text":
-                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-                break;
-            case "long":
-            case "int":
-                $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-                break;
-            case "double":
-                $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
-                break;
-            case "date":
-                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-                break;
-            case "defined":
-                $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-                break;
-        }
-        return $theValue;
+      switch ($theType) {
+        case "text":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;    
+        case "long":
+        case "int":
+          $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+          break;
+        case "double":
+          $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+          break;
+        case "date":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;
+        case "defined":
+          $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+          break;
+      }
+      return $theValue;
     }
 }
 
--- rent/login.php
+++ rent/login.php
@@ -1,31 +1,31 @@
 <?php require_once('Connections/cralwer.php'); ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
-    function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+    function GetSQLValueString($cralwer, $theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
     {
-        $theValue = @get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-        require_once('Connections/cralwer.php'); 
-        $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($cralwer,$theValue) : mysqli_escape_string($cralwer,$theValue);
+      $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
 
-        switch ($theType) {
-            case "text":
-                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-                break;
-            case "long":
-            case "int":
-                $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-                break;
-            case "double":
-                $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
-                break;
-            case "date":
-                $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-                break;
-            case "defined":
-                $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-                break;
-        }
-        return $theValue;
+      $theValue = function_exists("mysqli_real_escape_string") ? mysqli_real_escape_string($cralwer, $theValue) : mysqli_escape_string($cralwer, $theValue);`enter code here`
+
+      switch ($theType) {
+        case "text":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;    
+        case "long":
+        case "int":
+          $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+          break;
+        case "double":
+          $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+          break;
+        case "date":
+          $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+          break;
+        case "defined":
+          $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+          break;
+      }
+      return $theValue;
     }
 }
 
